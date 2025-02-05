1897f8ad1ee4298c815bfc31d756344624a767f0
Gabriel Melo <gabriel_smelo@proton.me>
2025-02-05 11:21:31 -0300

    fix: add --staged in script lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './components/App.tsx'
+import { App } from './components/App'
 
 // biome-ignore lint: noUnusedVariables
 createRoot(document.getElementById('root')!).render(
