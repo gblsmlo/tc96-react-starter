1af980c4bf494bacb248f8c5ccfced2002636d3f
Gabriel Melo <gabriel.smelo@proton.me>
2025-03-20 18:57:21 -0300

    chore: clear components and create minimal styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,16 @@
-import { Hero } from './Hero'
-
 export function App() {
 	return (
-		<div className="h-screen bg-primary p-10">
-			<Hero />
+		<div className="h-screen bg-neutral-900 p-10">
+			<div className='flex h-full flex-col items-center justify-center gap-2 text-center text-amber-400'>
+				<h1 className="font-extrabold">
+					<span className="text-2xl lg:text-6xl">tc</span>
+					<span className="text-2xl lg:text-4xl">⚡</span>
+					<span className="text-2xl lg:text-5xl">96</span>
+				</h1>
+				<h2 className="block font-extrabold text-2xl text-white lg:text-4xl">
+					React.js starter
+				</h2>
+			</div>
 		</div>
 	)
 }
--- src/components/SampleComponent.tsx
+++
@@ -1,7 +0,0 @@
-interface SampleComponentProps {
-	text: string
-}
-
-export function SampleComponent({ text }: SampleComponentProps) {
-	return <h1 className="text-neutral-100">{text}</h1>
-}
